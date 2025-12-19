3cbc6e1b9ec30def5b6bbd3c36907d093ab6b3f8
Samuel Andersen <samuel@andersentech.net>
2025-12-19 01:29:12 +0000

    Fix additional formatting errors

--- axlearn/cloud/gcp/jobset_utils.py
+++ axlearn/cloud/gcp/jobset_utils.py
@@ -1897,6 +1897,7 @@ class A4HighReplicatedJob(GPUReplicatedJob):
             },
         ]
 
+
 class A4XReplicatedJob(GPUReplicatedJob):
     """Builds a replicated job spec for an a4-high GPU job, to be used with JobSet API."""
 
@@ -1934,7 +1935,7 @@ class A4XReplicatedJob(GPUReplicatedJob):
                 resourceClaims=[
                     {
                         "name": "compute-domain-channel",
-                        "resourceClaimTemplateName": "a4x-compute-domain-channel"
+                        "resourceClaimTemplateName": "a4x-compute-domain-channel",
                     }
                 ],
                 affinity={
@@ -1946,16 +1947,14 @@ class A4XReplicatedJob(GPUReplicatedJob):
                                         {
                                             "key": "kubernetes.io/arch",
                                             "operator": "In",
-                                            "values": [
-                                                "arm64"
-                                            ]
+                                            "values": ["arm64"],
                                         }
                                     ]
                                 }
                             ]
                         }
                     }
-                }
+                },
             ),
         )
 
@@ -2033,12 +2032,8 @@ class A4XReplicatedJob(GPUReplicatedJob):
             # TODO(markblee): Improve SIGTERM behavior for command.
             command=command,
             resources=dict(
-                limits={
-                    "nvidia.com/gpu": "4"
-                },
-                claims=[
-                    {"name": "compute-domain-channel"}
-                ]),
+                limits={"nvidia.com/gpu": "4"}, claims=[{"name": "compute-domain-channel"}]
+            ),
             env=k8s_env_vars,
             volumeMounts=volume_mounts,
         )
