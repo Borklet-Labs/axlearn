99d87b4a2a24d5dc8b58b1ab8f204581cdef008f
Samuel Andersen <samuel@andersentech.net>
2026-05-13 21:05:10 +0000

    Fixing unit tests

--- axlearn/common/pipeline.py
+++ axlearn/common/pipeline.py
@@ -30,6 +30,7 @@ from typing import Callable, NamedTuple, Optional, Protocol, Union
 from jax import checkpoint
 from jax import numpy as jnp
 from jax.sharding import PartitionSpec
+from jax.ad_checkpoint import checkpoint_name
 
 from axlearn.common import param_init
 from axlearn.common.base_layer import (
@@ -550,7 +551,7 @@ class StreamSchedule(BaseSchedule):
 
             def compute_carry_input(v_input_t: Tensor, v_carry_output_t_1: Tensor) -> Tensor:
                 v_input_t = _select_input_or_previous_outputs(v_input_t, v_carry_output_t_1)
-                return jax.checkpoint_name(_shard_pipeline(v_input_t), "iter_input")
+                return checkpoint_name(_shard_pipeline(v_input_t), "iter_input")
 
             # Compute vmap inputs.
             # Leaves are of shape [N, microbatch_size, ...] representing per-stage inputs.
